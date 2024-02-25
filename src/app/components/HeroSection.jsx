6608b24c21f4d66e4855d1f4f60b3caabea1afa2
Lucasshao <yilongshao2020@gmail.com>
2024-02-25 22:31:56 +1100

    updated CV

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -59,7 +59,7 @@ const HeroSection = () => {
             </Link> */}
             <a
               download
-              href="/pdf/RESUME_LucasShao_12_02.pdf"
+              href="/pdf/RESUME_LucasShao_24_02.pdf"
               className="inline-block w-full px-1 py-1 mt-3 text-white rounded-full sm:w-fit bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800"
             >
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
